1862ac2e98096243e39e7d5c6e17a7e4e879c51a
chetansonawane <chetan.sonawane@kristasoft.com>
2024-04-03 17:03:03 +0530

    bugfix/KE-1546 refactor

--- outlook3/src/main/java/app/krista/extensions/essentials/collaboration/outlook3/api/OutlookApiResource.java
+++ outlook3/src/main/java/app/krista/extensions/essentials/collaboration/outlook3/api/OutlookApiResource.java
@@ -278,7 +278,7 @@ public final class OutlookApiResource {
             LOGGER.info("Test Connection successful. Saving attributes.");
             return createTestConnectionResponse(true, null, null);
         } catch (GraphServiceException cause) {
-            LOGGER.error("Failed to get data from graph service client.");
+            LOGGER.error("Failed to get data from graph service client. : {}",cause.getMessage());
             return createTestConnectionResponse(false, "An error occurred during test connection.", null);
         } catch (MustAuthorizeException cause) {
             String state = createStateParameter(cause, outlookAttributes);
