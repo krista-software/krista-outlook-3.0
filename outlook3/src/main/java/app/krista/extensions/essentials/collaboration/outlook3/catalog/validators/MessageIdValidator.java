327cdbbb8c95847e9bed94c0de5e6c4ed28b8df8
aradhyagudadhe <aradhya.gudadhe@kristasoft.com>
2024-07-16 09:59:24 +0530

    First raw commit

--- outlook3/src/main/java/app/krista/extensions/essentials/collaboration/outlook3/catalog/validators/MessageIdValidator.java
+++ outlook3/src/main/java/app/krista/extensions/essentials/collaboration/outlook3/catalog/validators/MessageIdValidator.java
@@ -25,9 +25,10 @@ public class MessageIdValidator implements Validator {
             account.getEmail(resourceId);
             return true;
         } catch (MustAuthorizeException cause) {
+            logger.error(cause.getMessage(), cause);
             throw cause;
         } catch (Exception cause) {
-            logger.info(cause.getMessage());
+            logger.info(cause.getMessage(),cause);
             return false;
         }
     }
